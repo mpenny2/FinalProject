146a8640a929d0ff9f4f6347d3aeb9723b254b6d
gregoryesrig24 <gesrig@nd.edu>
2017-04-11 17:22:55 -0400

    Second Commit

---
+++ app/src/main/java/com/example/gregoryesrig24/finalproject/GiveProfile.java
@@ -0,0 +1,22 @@
+package com.example.gregoryesrig24.finalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by gregoryesrig24 on 4/10/17.
+ */
+
+public class GiveProfile extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giveprofile);
+
+    }
+
+
+
+}
