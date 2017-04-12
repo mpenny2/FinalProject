9e2e1a0dbc899bc6eef2132b7d30595689df7450
mpenny2 <mpenny2@nd.edu>
2017-04-12 10:14:00 -0400

    Map image

--- app/src/main/java/com/example/gregoryesrig24/finalproject/GetProfile.java
+++ app/src/main/java/com/example/gregoryesrig24/finalproject/GetProfile.java
@@ -1,7 +1,12 @@
 package com.example.gregoryesrig24.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import static com.example.gregoryesrig24.finalproject.R.id.getButton;
 
 /**
  * Created by gregoryesrig24 on 4/10/17.
@@ -9,9 +14,19 @@ import android.support.v7.app.AppCompatActivity;
 
 public class GetProfile extends AppCompatActivity {
 
+    private Button getButton;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_getprofile);
+        getButton = (Button) findViewById(R.id.getButton);
+
+
+        getButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GetProfile.this, SelectionActivity.class));
+            }
+        });
 
     }
 }
