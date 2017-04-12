58ad0420ea079e05c6759d696440c787597abcfb
mpenny2 <mpenny2@nd.edu>
2017-04-11 23:03:23 -0400

    Expanded List

--- app/src/main/java/com/example/gregoryesrig24/finalproject/GiveProfile.java
+++ app/src/main/java/com/example/gregoryesrig24/finalproject/GiveProfile.java
@@ -6,17 +6,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import static android.R.attr.button;
+
 /**
  * Created by gregoryesrig24 on 4/10/17.
  */
 
 public class GiveProfile extends AppCompatActivity {
+
+    private Button givingbtn;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giveprofile);
 
-    }
+        givingbtn = (Button) findViewById(R.id.giveButton);
 
+
+        givingbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GiveProfile.this, SelectionActivity.class));
+            }
+        });
+
+
+
+
+
+
+    }
 
 
 }
