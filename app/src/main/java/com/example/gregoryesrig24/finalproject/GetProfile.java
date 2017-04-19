3664793ad53549b36748ddda8e55e94a436ab223
mpenny2 <mpenny2@nd.edu>
2017-04-18 23:43:15 -0400

    Map image

--- app/src/main/java/com/example/gregoryesrig24/finalproject/GetProfile.java
+++ app/src/main/java/com/example/gregoryesrig24/finalproject/GetProfile.java
@@ -3,6 +3,10 @@ package com.example.gregoryesrig24.finalproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +15,7 @@ import static com.example.gregoryesrig24.finalproject.R.id.getButton;
 /**
  * Created by gregoryesrig24 on 4/10/17.
  */
+import static com.example.gregoryesrig24.finalproject.R.id.toolbar;
 
 public class GetProfile extends AppCompatActivity {
 
@@ -28,5 +33,27 @@ public class GetProfile extends AppCompatActivity {
             }
         });
 
+        Toolbar actionBarToolbar = (Toolbar) findViewById(toolbar);
+        setSupportActionBar(actionBarToolbar);
+
+        getSupportActionBar().setTitle("");
+
+    }
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int res_id = item.getItemId();
+
+        if (res_id == R.id.settings) {
+            startActivity(new Intent(GetProfile.this, MainActivity.class));
+        }
+
+        return true;
     }
 }
+
